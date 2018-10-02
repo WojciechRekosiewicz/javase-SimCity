60d5a3a56270f049d0f5a97f8c07bbe825185540
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-10-02 14:25:00 +0200

    setTheImageOfTheCar class added and image of cars added

--- src/main/java/sincity/model/Vehicle.java
+++ src/main/java/sincity/model/Vehicle.java
@@ -1,5 +1,7 @@
 package sincity.model;
 
+import javafx.scene.image.Image;
+
 import java.util.Random;
 
 public class Vehicle {
@@ -10,6 +12,15 @@ public class Vehicle {
     private int[] puzzle;
     private double roadPosition;
     private int image;
+    private Image carImage;
+
+    public Image getCarImage() {
+        return carImage;
+    }
+
+    public void setCarImage(Image carImage) {
+        this.carImage = carImage;
+    }
 
     public int getImage() {
         return image;
@@ -28,6 +39,37 @@ public class Vehicle {
         Random generator = new Random();
         image = generator.nextInt(7);
         setImage(image);
+    }
+
+    private void setTheImageOfTheCar() {
+        int numOfImage = getImage();
+
+        if (numOfImage == 0) {
+            Image carImage = new Image("car_00.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 1) {
+            Image carImage = new Image("car_01.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 2) {
+            Image carImage = new Image("car_02.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 3) {
+            Image carImage = new Image("car_03.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 4) {
+            Image carImage = new Image("car_04.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 5) {
+            Image carImage = new Image("car_05.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 6) {
+            Image carImage = new Image("car_06.png");
+            setCarImage(carImage);
+        } else if (numOfImage == 7) {
+            Image carImage = new Image("car_07.png");
+            setCarImage(carImage);
+        }
+
     }
 
     private void chooseDirection() {
