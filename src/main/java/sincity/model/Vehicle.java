ccab4af70d6949212720e72c235630cdbc931e9b
tomaszgubernat <tomekgubernat@gmail.com>
2018-10-03 13:32:52 +0200

    short function setTheImageOfTheCar and new inheriting vehicle class

--- src/main/java/sincity/model/Vehicle.java
+++ src/main/java/sincity/model/Vehicle.java
@@ -5,23 +5,19 @@ import javafx.scene.image.Image;
 import java.util.Random;
 
 public class Vehicle {
-    private double MAX_SPEED;
-    private double speed;
-    private boolean size;
-    private int[] puzzle;
-    private double roadPosition;
+    double maxSpeed;
+    double speed;
+    boolean size;
+    int[] puzzle;
+    double roadPosition;
     private int imageNumber;
     private Image carImage;
-
-    private void move() {
-
-    }
 
     public Image getCarImage() {
         return carImage;
     }
 
-    public void setCarImage(Image carImage) {
+    private void setCarImage(Image carImage) {
         this.carImage = carImage;
     }
 
@@ -33,53 +29,9 @@ public class Vehicle {
         this.imageNumber = imageNumber;
     }
 
-
-    enum direction {
-        NORTH(),
-        SOUTH(),
-        WEST(),
-        EAST()
+    private void setTheImageOfTheCar(VehicleType vehicleType, int numberOfVehicle) {
+        int randomImageNumber = (int) Math.floor(Math.random() * numberOfVehicle);
+        Image carImage = new Image(vehicleType.getName() + "_0" + randomImageNumber + ".png");
+        setCarImage(carImage);
     }
-
-    private void chooseImageRandomly() {
-        //   int imageNumber = getImageNumber();
-        Random generator = new Random();
-        imageNumber = generator.nextInt(7);
-        setImageNumber(imageNumber);
-    }
-
-    private void setTheImageOfTheCar() {
-        int imageNumber = getImageNumber();
-
-        if (imageNumber == 0) {
-            Image carImage = new Image("car_00.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 1) {
-            Image carImage = new Image("car_01.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 2) {
-            Image carImage = new Image("car_02.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 3) {
-            Image carImage = new Image("car_03.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 4) {
-            Image carImage = new Image("car_04.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 5) {
-            Image carImage = new Image("car_05.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 6) {
-            Image carImage = new Image("car_06.png");
-            setCarImage(carImage);
-        } else if (imageNumber == 7) {
-            Image carImage = new Image("car_07.png");
-            setCarImage(carImage);
-        }
-    }
-
-    private void chooseDirection() {
-
-    }
-
 }
---
+++ src/main/java/sincity/model/VehicleType.java
@@ -0,0 +1,19 @@
+package sincity.model;
+
+public enum VehicleType {
+
+    CAR("car"),
+    TRUCK("truck"),
+    MOTOR("motor");
+
+    private final String NAME;
+
+    VehicleType(String name) {
+        this.NAME = name;
+    }
+
+    String getName() {
+        return this.NAME;
+    }
+
+}
