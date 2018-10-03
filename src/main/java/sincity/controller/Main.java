4d9e2f9094bade5419d9e707493c5689d2b70b98
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-10-03 10:40:54 +0200

    added basic application template

--- src/main/java/sincity/controller/Main.java
+++ src/main/java/sincity/controller/Main.java
@@ -1,33 +1,42 @@
 package sincity.controller;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sincity.model.City;
 import sincity.view.Renderer;
 
 public class Main extends Application {
-    private int verticalPuzzles = 3;
-    private int horizontalPuzzles = 3;
 
-    private double sceneWidth = 800;
-    private double sceneHeight = 800;
+    // board size, there should be always more horizontalPuzzles than verticalPuzzles
+    private int horizontalPuzzles = 10;
+    private int verticalPuzzles = 5;
 
-    private double tileWidth = sceneWidth / verticalPuzzles;
-    private double tileHeight = sceneHeight / horizontalPuzzles;
+    private double sceneWidth = 800; // window size in pixels
+    private double tileSize = sceneWidth / horizontalPuzzles; // tile size in pixels
+    private double sceneHeight = tileSize * verticalPuzzles; // sceneHeight automatically adjusts based on tileSize and sceneWidth
 
     @Override
     public void start(Stage primaryStage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, sceneWidth, sceneHeight);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        if (horizontalPuzzles >= verticalPuzzles) {
+            Group root = new Group();
+            Scene scene = new Scene(root, sceneWidth, sceneHeight);
+            primaryStage.setScene(scene);
+            primaryStage.show();
 
-        int padding = 1; // tiles off-screen
+            int padding = 1; // tiles off-screen
 
-        // tutaj potrzebne stworzenie City city z parametrami (verticalTiles, horizontalTiles, padding)
+            // create city with desired size
+            City city = new City(verticalPuzzles, horizontalPuzzles, padding, tileSize);
 
-        Renderer renderer = new Renderer(city, root, tileWidth, tileHeight, verticalPuzzles, horizontalPuzzles, padding);
-        renderer.render();
+            // create view based on city
+            Renderer renderer = new Renderer(root, city, tileSize, verticalPuzzles, horizontalPuzzles, padding);
+            renderer.render();
+        } else {
+            System.out.println("Wrong board size, please make sure to have more horizontal puzzles than vertical ones.");
+            Platform.exit();
+        }
     }
 }
