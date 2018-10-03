ab96808a8fca0f1fed7be6615eae7444782c9c11
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-10-03 14:50:40 +0200

    added example road

--- src/main/java/sincity/model/Vehicle.java
+++ src/main/java/sincity/model/Vehicle.java
@@ -6,10 +6,7 @@ import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
 import javafx.util.Duration;
 
 import java.util.Random;
@@ -23,29 +20,20 @@ public class Vehicle {
     private int imageNumber;
     private Image carImage;
 
-    private void move() {
-//        Rectangle rectangle = new Rectangle(15, 10);
-//        rectangle.setFill(Color.GREEN);
-//        Path path = new Path();
-//        MoveTo moveTo = new MoveTo(10, 10);
-//        LineTo line1 = new LineTo(100, 10);
-//        LineTo line2 = new LineTo(100, 100);
-//        LineTo line3 = new LineTo(10, 100);
-//        LineTo line4 = new LineTo(10, 10);
-//        LineTo line5 = new LineTo(300, 250);
-//        path.getElements().add(moveTo);
-//        path.getElements().addAll(line1, line2, line3, line4);
-//        PathTransition pathTransition = new PathTransition();
-//        pathTransition.setDuration(Duration.seconds(10));
-//        pathTransition.setNode(rectangle);
-//        pathTransition.setPath(path);
-//        pathTransition.setOrientation(
-//                PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-//        pathTransition.setCycleCount(5000);
-//        pathTransition.setAutoReverse(false);
-//        pathTransition.play();
-
+    private void move(Line path) {
+        Rectangle rectangle = new Rectangle(15, 10);
+        rectangle.setFill(Color.GREEN);
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.seconds(15));
+        pathTransition.setNode(rectangle);
+        pathTransition.setPath(path);
+        pathTransition.setOrientation(
+                PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(5000);
+        pathTransition.setAutoReverse(false);
+        pathTransition.play();
     }
+
 
     private void timeline() {
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(10), ev -> {
