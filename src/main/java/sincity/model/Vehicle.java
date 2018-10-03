e54ec1201a18f4297e06af208073a29cc98f9fe5
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-10-03 13:56:45 +0200

    added comment

--- src/main/java/sincity/model/Vehicle.java
+++ src/main/java/sincity/model/Vehicle.java
@@ -24,26 +24,26 @@ public class Vehicle {
     private Image carImage;
 
     private void move() {
-        Rectangle rectangle = new Rectangle(15, 10);
-        rectangle.setFill(Color.GREEN);
-        Path path = new Path();
-        MoveTo moveTo = new MoveTo(10, 10);
-        LineTo line1 = new LineTo(100, 10);
-        LineTo line2 = new LineTo(100, 100);
-        LineTo line3 = new LineTo(10, 100);
-        LineTo line4 = new LineTo(10, 10);
-        LineTo line5 = new LineTo(300, 250);
-        path.getElements().add(moveTo);
-        path.getElements().addAll(line1, line2, line3, line4);
-        PathTransition pathTransition = new PathTransition();
-        pathTransition.setDuration(Duration.seconds(10));
-        pathTransition.setNode(rectangle);
-        pathTransition.setPath(path);
-        pathTransition.setOrientation(
-                PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-        pathTransition.setCycleCount(5000);
-        pathTransition.setAutoReverse(false);
-        pathTransition.play();
+//        Rectangle rectangle = new Rectangle(15, 10);
+//        rectangle.setFill(Color.GREEN);
+//        Path path = new Path();
+//        MoveTo moveTo = new MoveTo(10, 10);
+//        LineTo line1 = new LineTo(100, 10);
+//        LineTo line2 = new LineTo(100, 100);
+//        LineTo line3 = new LineTo(10, 100);
+//        LineTo line4 = new LineTo(10, 10);
+//        LineTo line5 = new LineTo(300, 250);
+//        path.getElements().add(moveTo);
+//        path.getElements().addAll(line1, line2, line3, line4);
+//        PathTransition pathTransition = new PathTransition();
+//        pathTransition.setDuration(Duration.seconds(10));
+//        pathTransition.setNode(rectangle);
+//        pathTransition.setPath(path);
+//        pathTransition.setOrientation(
+//                PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+//        pathTransition.setCycleCount(5000);
+//        pathTransition.setAutoReverse(false);
+//        pathTransition.play();
 
     }
 
