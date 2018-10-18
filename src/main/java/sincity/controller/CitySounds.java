223a440de186976599cb48d3d8cf609dd31f92d4
Wirek90 <annawirecka@gmail.com>
2018-10-18 12:22:37 +0200

    add mp3 files

--- src/main/java/sincity/controller/CitySounds.java
+++ src/main/java/sincity/controller/CitySounds.java
@@ -9,7 +9,7 @@ import java.io.File;
 public class CitySounds {
 
     public static void playSounds() {
-        String musicFile = "src/main/resources/citySounds.mp3";
+        String musicFile = "src/main/resources/citySounds.mp3.mp3";
         Media sound = new Media(new File(musicFile).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(sound);
         mediaPlayer.play();
